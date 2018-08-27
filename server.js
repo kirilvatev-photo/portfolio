52fdf74c229f3cb94a2ca40f3907544ea421e038
Kiril Vatev <vatev.1@gmail.com>
2018-08-27 00:17:38 -0400

    adding server logging

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+});
+
 app.use(express.static('public'));
 app.use('/images', express.static('images'));
 
